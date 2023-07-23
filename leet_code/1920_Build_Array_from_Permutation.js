d96a28430ee310c9763ee60c6c00a60e4d0ad141
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-07-23 12:36:33 +0530

    Create 1920_Build_Array_from_Permutation.js
    https://leetcode.com/problems/build-array-from-permutation/description/

---
+++ leet_code/1920_Build_Array_from_Permutation.js
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/build-array-from-permutation/description/
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var buildArray = function(nums) {
+    let res = [];
+    for(let i=0; i<nums.length; i++) {
+        res.push(nums[nums[i]]);
+    }
+    return res;
+};
