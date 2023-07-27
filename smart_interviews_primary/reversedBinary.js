77c225674f567bdbf1ae1b120193d80e43e387a3
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-07-27 20:06:21 +0530

    Create reversedBinary.js
    https://www.hackerrank.com/contests/smart-interviews/challenges/si-reverse-bits/problem

---
+++ smart_interviews_primary/reversedBinary.js
@@ -0,0 +1,61 @@
+// https://www.hackerrank.com/contests/smart-interviews/challenges/si-reverse-bits/problem
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'reversedBinary' function below.
+ */
+
+function reversedBinary(num) {
+    // Write your code here
+    let reversed = BigInt(0);
+    const one = BigInt(1);
+    let count = 32;
+    
+    while(count > 0) {
+        reversed = reversed << one;
+        const setBit = (num & one) == one;
+        if(setBit) {
+            reversed = reversed | one;
+        }
+        num = num >> one;
+        count--;
+    }
+    return reversed;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const T = parseInt(readLine().trim(), 10);
+    for(let i = 0; i<T; i++) {
+        const number = BigInt(readLine().replace(/\s+$/g, ''));
+        const result = reversedBinary(number);
+        ws.write(result + '\n');
+    }
+
+
+    ws.end();
+}
