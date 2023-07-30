7948dc5d813b3fee9feaa8921154492866f9504c
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-07-30 11:21:43 +0530

    Update 1_twoSum.js
    added another solution

--- leet_code/1_twoSum.js
+++ leet_code/1_twoSum.js
@@ -6,11 +6,30 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i=0; i<nums.length; i++) {
-        for(let j=0; j<nums.length; j++) {
-            if(i!=j && nums[i]+nums[j] == target) {
-                return [i,j];
-            }
+    // time complexity: O(n^2), space complexity: O(1)
+    // for(let i=0; i<nums.length; i++) {
+    //     for(let j=0; j<nums.length; j++) {
+    //         if(i!=j && nums[i]+nums[j] == target) {
+    //             return [i,j];
+    //         }
+    //     }
+    // }
+
+    // time complexity: O(n) + O(n), space complexity: O(n)
+    let dict = {};
+    for (let i=0; i<nums.length;  i++) {
+        if(dict.hasOwnProperty(nums[i])) {
+            dict[nums[i]] = [dict[nums[i]], i];
+        } else {
+            dict[nums[i]] = i;
         }
-    }
+    };
+    for (let i of nums) {
+        if(dict[target-i]){
+            if(Array.isArray(dict[target-i]))
+                return dict[i];
+            else if (dict[i] != dict[target-i]) {
+                return [dict[i], dict[target-i]];
+        }}
+    };
 };
