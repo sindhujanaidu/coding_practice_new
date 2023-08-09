daa77bbc17abf538c11d0aced21e234942712477
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-08-09 17:51:23 +0530

    Update 509_fibonacci.js

--- leet_code/509_fibonacci.js
+++ leet_code/509_fibonacci.js
@@ -6,6 +6,7 @@
  * @param {number} n
  * @return {number}
  */
+
 var fib = function(n) {
     if(n == 0) {
         return 0;
@@ -14,4 +15,28 @@ var fib = function(n) {
     } else {
         return fib(n-1) + fib(n-2);
     }
+};
+
+// solution 2 using dict
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fibonacci = function(n, obj) {
+    if(n == 0) {
+        obj["0"] = 0;
+        return 0;
+    } else if(n == 1 || n == 2) {
+        obj["1"] = 1;
+        obj["2"] = 1;
+        return 1;
+    } else {
+        if(obj[n]) return obj[n];
+        x = fibonacci(n-1, obj) + fibonacci(n-2, obj);
+        obj[n] = x;
+        return x;
+    }
+}
+var fib = function(n) {
+    return fibonacci(n, {});
 };
