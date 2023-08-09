2eb16c76a50c286c26c9c77b9e52019184c10cc0
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-08-09 17:38:59 +0530

    Create 509_fibonacci.js
    https://leetcode.com/problems/fibonacci-number/description/

---
+++ leet_code/509_fibonacci.js
@@ -0,0 +1,17 @@
+// https://leetcode.com/problems/fibonacci-number/description/
+
+//recursion
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var fib = function(n) {
+    if(n == 0) {
+        return 0;
+    } else if(n == 1 || n == 2) {
+        return 1;
+    } else {
+        return fib(n-1) + fib(n-2);
+    }
+};
