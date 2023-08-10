4943bd4ff29ddafc58a699fb2f1fd839a34333ed
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-08-10 11:16:23 +0530

    Create 326_powerOfThree.js
    https://leetcode.com/problems/power-of-three/description/

---
+++ leet_code/326_powerOfThree.js
@@ -0,0 +1,9 @@
+// https://leetcode.com/problems/power-of-three/description/
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    return (n>0 && 1162261467%n==0);
+};
