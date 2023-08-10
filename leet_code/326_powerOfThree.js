29b5570ea731685dc39426881a1841a157186cda
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-08-10 11:21:02 +0530

    Update 326_powerOfThree.js
    comments added

--- leet_code/326_powerOfThree.js
+++ leet_code/326_powerOfThree.js
@@ -5,5 +5,8 @@
  * @return {boolean}
  */
 var isPowerOfThree = function(n) {
-    return (n>0 && 1162261467%n==0);
+    // console.log("2 power 31 ", 2**(31)) //2147483648
+    // console.log("3 power 20 ", 3**(20)) //3486784401
+    // console.log("3 power 19 ", 3**(19)) //1162261467
+    return (n>0 && 1162261467%n == 0);
 };
