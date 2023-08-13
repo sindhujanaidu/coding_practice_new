3f7994cfdf3cc0ad3542a4d0c951c730bd392a07
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-08-13 16:08:04 +0530

    Create sumAndXor.js
    question added

---
+++ smart_interview_contest/sumAndXor.js
@@ -0,0 +1,38 @@
+Sum and XOR
+Given a integer N, find the number of positive integers X such that X<=N and N+X = N^X (N xor X).
+
+Input
+First line of input contains T - number of test cases. Its followed by T lines, each line contains a single integer N.
+
+Output
+For each test case, print the count of X's, separated by new line.
+
+Constraints
+20 points
+1 <= T <= 103
+0 <= N <= 103
+
+80 points
+1 <= T <= 104
+0 <= N <= 1018
+
+Example
+Input
+2
+5
+10
+
+Output
+1
+3
+
+Explanation
+
+Test Case 1
+Possible values: 5+2 = 5^2.
+
+Test Case 2
+Possible values: 10+1 = 10^1, 10+4=10^4, 10+5=10^5
+
+//solution
+
