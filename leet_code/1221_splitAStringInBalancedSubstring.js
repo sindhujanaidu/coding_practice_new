7369bfae920e9556faffcce5dbf1516eafbcfdda
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-09-06 18:18:51 +0530

    Create 1221_splitAStringInBalancedSubstring.js
    https://leetcode.com/problems/split-a-string-in-balanced-strings/description/

---
+++ leet_code/1221_splitAStringInBalancedSubstring.js
@@ -0,0 +1,18 @@
+// https://leetcode.com/problems/split-a-string-in-balanced-strings/description/
+
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var balancedStringSplit = function(s) {
+    let balance = 0;
+    let result = 0;
+
+    for (const i of s) {
+        if (i === 'L') balance--;
+        else balance++;
+        if (balance === 0) result++;
+    }
+
+    return result;
+};
