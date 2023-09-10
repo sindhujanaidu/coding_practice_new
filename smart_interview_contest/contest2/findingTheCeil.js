71884398c8ea9b588a520fae5b45de267e134cfe
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-09-10 22:34:03 +0530

    Create findingTheCeil.js

---
+++ smart_interview_contest/contest2/findingTheCeil.js
@@ -0,0 +1,50 @@
+/*
+Finding the Ceil
+Given an array, you have to find the ceil of a number x. The ceil of a number x is nothing but the smallest number in the array greater than or equal to x.
+
+
+Input Format
+
+The first line of input contains the N-size of the array. The next line contains N integers, the elements of the array. The next line contains Q - number of queries. 
+Each of the next Q lines contains a single integer X, for which you have to find the ceil of X in the given array.
+
+Output Format
+For each query, print the ceil of X, separated by a new line. If ceil is not found, print the value of "INT_MAX
+
+
+Constraints
+
+30 points
+1 <= N <=105
+1 <= Q <=102
+-108 <= ar[i] <=108
+
+70 points
+1 <= N <=105
+1 <= Q <=105
+-108 <= ar[i] <=108
+
+Example
+
+Input
+6
+-6 10 -1 20 15 5
+5
+-1
+10
+13
+25
+-10
+
+
+Output
+-1
+10
+15
+2147483647
+-6
+
+
+Explanation
+Self Explanatory
+*/
