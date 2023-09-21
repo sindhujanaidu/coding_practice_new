77fd1ff8f78972c593d0ce80bdaf27ec37d9d3aa
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-09-21 17:16:39 +0530

    Create 206_reversedLinkedList.js

---
+++ leet_code/206_reversedLinkedList.js
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/reverse-linked-list/description/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+function ListNode(val, next) {
+     this.val = (val===undefined ? 0 : val)
+     this.next = (next===undefined ? null : next)
+}
+var reverseList = function(head) {
+    // sol 1
+    let c = head;
+    if(head == null) return head;
+    let rev;
+    while(c != null) {
+        if(rev == null) {
+            rev = new ListNode(c.val);
+        } else {
+            rev = new ListNode(c.val, rev);
+        }
+        c = c.next;
+    }
+    return rev;
+
+  // sol 2
+};
