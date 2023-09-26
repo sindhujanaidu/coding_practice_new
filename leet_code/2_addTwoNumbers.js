781e50f0211d4d42f71c620ff7fdee8d496da309
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-09-26 22:37:27 +0530

    Update 2_addTwoNumbers.js
    sol 2 added

--- leet_code/2_addTwoNumbers.js
+++ leet_code/2_addTwoNumbers.js
@@ -61,3 +61,42 @@ var addTwoNumbers = function(l1, l2) {
     }
     return temp.next;
 };
+
+/// solution 2 - without extra LL
+var addTwoNumbers = function(h1, h2) {
+    let h = h1, c = 0;
+    let temp = h;
+    while(h1 != null && h2 != null) {
+        s = h1.val + h2.val + c;
+        c = 0;
+        if(s > 9) c = 1;
+        h1.val = s%10;
+        temp = h1;
+        h1 = h1.next;
+        h2 = h2.next;
+    }
+
+    while(h1 != null) {
+        s = h1.val + c;
+        c = 0;
+        if(s > 9) c = 1;
+        h1.val = s%10;
+        temp = h1;
+        h1 = h1.next;
+    }
+
+    while(h2 != null) {
+        s = h2.val + c;
+        c = 0;
+        if(s > 9) c = 1;
+        h2.val = s%10;
+        temp.next = h2;
+        temp = temp.next;
+        h2 = h2.next;
+    }
+    if(c == 1) {
+        temp.next = new ListNode(c)
+        temp = temp.next;
+    }
+    return h;
+}
