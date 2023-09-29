9a1601583ab15d87d9f80dde3bd41e055caf92fe
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-09-29 20:40:21 +0530

    Create 142_linkedListCycle2.js
    slow fast approach

---
+++ leet_code/142_linkedListCycle2.js
@@ -0,0 +1,31 @@
+// https://leetcode.com/problems/linked-list-cycle-ii/description/
+
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var detectCycle = function(head) {
+    let slow = head;
+    let fast = head;
+    while (fast && fast.next) {
+        slow = slow.next;
+        fast = fast.next.next;
+        if (slow === fast) {
+            let ptr = head;
+            while (ptr !== slow) {
+                ptr = ptr.next;
+                slow = slow.next;
+            }
+            return ptr;
+        }
+    }
+    return null;
+};
