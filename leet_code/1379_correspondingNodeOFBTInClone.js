7e5598162e48f83fc4cf73938c0ac230e23648a0
sindhujanaidu <140299387+sindhujanaidu@users.noreply.github.com>
2023-10-14 17:42:44 +0530

    Create 1379_correspondingNodeOFBTInClone.js

---
+++ leet_code/1379_correspondingNodeOFBTInClone.js
@@ -0,0 +1,24 @@
+// https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/description/
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+
+var getTargetCopy = function(original, cloned, target) {
+    if(original == null) return null;
+    if(original == target) return cloned;
+    return (
+        getTargetCopy(original.left, cloned.left, target) ||
+        getTargetCopy(original.right, cloned.right, target)
+    );
+};
